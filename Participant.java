b06fcd14c5e1621a9e900565eceb118c41797e95
Efe Berk Dağlı <efedagli06@gmail.com>
2026-04-03 00:15:52 +0300

    Player, Participant classes
    Created Player and Participant classes

---
+++ Participant.java
@@ -0,0 +1,17 @@
+public abstract class Participant {
+    protected String name;
+    protected int id;
+
+    public Participant(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ Player.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Player extends Participant {
+
+    protected Map<String, Integer> attributes;
+
+    public Player(String name, int id) {
+        super(name, id);
+        this.attributes = new HashMap<>();
+    }
+
+
+    public void setAttribute(String skillName, int value) {
+        this.attributes.put(skillName, value);
+    }
+
+
+    public int getAttribute(String skillName) {
+        return this.attributes.getOrDefault(skillName, 0);
+    }
+
+
+    public Map<String, Integer> getAllAttributes() {
+        return this.attributes;
+    }
+}
