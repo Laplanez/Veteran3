cc231b396c8c8941ce42019faa896fa5ca5b0cfd
Efe Berk Dağlı <efedagli06@gmail.com>
2026-04-09 00:31:54 +0300

    Main improve
    In main class, league and teams were created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=== Sports Manager: Milestone 2 Simulation ===");
 
-        System.out.printf("maven implemented");
-
-
+       //League and teams of the league were created
+        League league = new League();
+        Team t1 = new Team("Veteran FC");
+        Team t2 = new Team("Eagle United");
+        Team t3 = new Team("Lion City");
+        Team t4 = new Team("Shark Base");
     }
 }
